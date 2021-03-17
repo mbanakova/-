1fff4cf5980839b2a0da8a896d13aadecb2bd5c1
Маша <mbanakova@yandex.ru>
2021-03-17 04:39:46 +0300

    first commit

---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+
+const items = document.querySelectorAll('.accordion__item');
+
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    const title = item.querySelector('.accordion__title');
+    const height = title.nextElementSibling.scrollHeight;
+    title.classList.toggle('accordion__title--active');
+    if (title.classList.contains('accordion__title--active')) {
+      title.nextElementSibling.style.maxHeight = `${height}px`;
+    } else {
+      title.nextElementSibling.style.maxHeight = 0;
+    }
+  })
+});
